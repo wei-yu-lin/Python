6dc5351ea597c0cdd57a53b93cface35b155fd99
wei-yu-lin <a3815612cxv@gmail.com>
2020-07-09 00:14:28 +0800

    更新template for迴圈bug

--- handle_csv.py
+++ handle_csv.py
@@ -15,26 +15,27 @@ def lotto():
     for i in range(0,7):    
         for j in range(1,7):             
             savage = "Lotto649Control_history_dlQuery_SNo"+str(j)+"_"+str(i)            
-            sub_list = savage[0:]
+            sub_list = savage[0:]           
             list_result = soup.find(id=sub_list).string
-            soup_no.append(list_result)               
-    return soup_no[0:]
+            soup_no.append(list_result)  
+    lotto_item=[]  
+    seq_keep = []               
+    for seq in range(0,len(soup_no),6):                     
+        store_lotto = soup_no[seq:seq+6]             
+        lotto_item.append(store_lotto)  
+        seq_keep.append(seq)             
+    return lotto_item,seq_keep
             
 if __name__ == '__main__':
-    TEMPLATE = "./樂透爬蟲 寫入html/template.html"
-    INDEX_HTML = "./樂透爬蟲 寫入html/index.html"
-
-    
-
-    with open(INDEX_HTML, 'wb') as f:        
-        lottolist=lotto()    
-        #lottostr = "\n".join('%s' %id for id in lottolist)
-        for seq in  range(0,len(lottolist),6):
-            
-            store_lotto=lottolist[seq:seq+6]
-            html = template(TEMPLATE, items=store_lotto , seq = int((seq/6)+1))  
-            f.write(html.encode("utf-8"))     
-       # breakpoint()
+    TEMPLATE = "./template.html"
+    INDEX_HTML = "./index.html"
+    lotto_function = lotto()
+    lotto_item = lotto_function[0]
+    seq_keep = lotto_function[1]
+    print(lotto_item)
+    with open(INDEX_HTML, 'wb') as f:
+        html = template(TEMPLATE, items=lotto_item)  
+        f.write(html.encode("utf-8"))     
         print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
         print("=======success========")
         f.close()     
